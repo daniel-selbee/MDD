1cd6bfed0d180d605af5bcc14da57e72c8ba58e6
Daniel Selbee <sulb1210@yahoo.com>
2014-04-09 15:47:10 -0600

    View For Listing, View for Details

--- js/app.js
+++ js/app.js
@@ -3,9 +3,14 @@ var myApp = angular.module('myApp', ['ngRoute', 'movieControllers']);
 myApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/list',{
-        templateUrl: 'partials/lists.html',
-        controller: 'ListController'
-    }).otherwise({
-            redirectTo:'/list'
-        });
+            templateUrl: 'partials/lists.html',
+            controller: 'ListController'
+        }).
+        when('/movie/:id',{
+            templateUrl: 'partials/movie.html',
+            controller: 'DetailsController'
+        }).
+        otherwise({
+                redirectTo:'/list'
+            });
 }])
--- js/controllers.js
+++ js/controllers.js
@@ -8,13 +8,30 @@ movieControllers.controller('ListController', function($scope, $http){
             callback: 'JSON_CALLBACK',
             total: 50,
             page_limit: 15
-
         }
     })
         .success(function (data) {
             $scope.movies = data;
             console.log(data);
+        });
 
+
+});
+movieControllers.controller('DetailsController', function($scope, $http, $routeParams){
+
+    $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies/' + $routeParams.id + '.json'
+
+    , {
+        params: {
+            apikey: 't47cwspvrd5pwh234nkbgakx',
+            callback: 'JSON_CALLBACK',
+            total: 50,
+            page_limit: 15
+        }
+    })
+        .success(function (data) {
+            $scope.singleMovies = data;
+            console.log(data.title);
         });
 
 
